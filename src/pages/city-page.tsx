de064d274cc562477ffd02e044016120c7f1270f
Rishnu DK <rishnukrishnan@gmail.com>
2025-08-15 00:13:59 +0530

    added logo

---
+++ src/pages/city-page.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const CityPage = () => {
+  return (
+    <div>City-page</div>
+  )
+}
+
+export default CityPage
---
+++ src/pages/weather-dashboard.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const WeatherDashboard = () => {
+  return (
+    <div>WeatherDashboard</div>
+  )
+}
+
+export default WeatherDashboard
