33f6035a1f727a51210edd62f2fb5c86f8716df9
Rishnu DK <rishnukrishnan@gmail.com>
2025-09-23 23:04:43 +0530

    added location error

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,21 +1,50 @@
 
 import React from 'react'
 import {Button} from "@/components/ui/button"
-import {RefreshCw} from "lucide-react";
+import {AlertTriangle, MapPin, RefreshCw} from "lucide-react";
 import { useGeolocation } from '../hooks/use-geolocation';
+import WeatherSkeleton from '../components/ui/loading-skeleton';
+import { Alert, AlertDescription, AlertTitle } from '../components/ui/alert';
 
 const WeatherDashboard = () => {
 
-  const { coordinates, error : locationError, getLocation, isLoading : locationLoading} = useGeolocation()
+  const { coordinates,
+     error : locationError,
+      getLocation,
+       isLoading : locationLoading}
+        = useGeolocation()
   console.log(coordinates);
 
-  const handlerefresh = () => {
+  const handleRefresh = () => {
     getLocation();
     if(coordinates) {
       //reload weather data
     }
   }
   
+
+  if (locationLoading) {
+    return  <WeatherSkeleton/>
+  }
+
+  if (locationError) {
+     
+      return  (
+      <Alert variant="destructive">
+        <AlertTriangle className='h-4 w-4' />
+        <AlertTitle>Location Error</AlertTitle>
+        <AlertDescription className='flex flex-col gap-4'>
+          <p>{locationError}</p>
+          <Button onClick= {getLocation} varient={"outline"} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4"/>
+            Enable Location
+          </Button>
+          
+        </AlertDescription>
+      </Alert>
+      )
+    
+  }
 
   return (
     <div className='space-y-4' >
